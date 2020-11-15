2831720b419843b689d92efc21d1f924ebd4d565
NaramonRhino <ultimate.nafis.games@gmail.com>
2020-11-15 10:48:43 +0600

    fixing some n00b fkups

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := device/amazon/karnak
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Inherit from the proprietary version
-include vendor/amazon/BoardConfigVendor.mk
+include vendor/amazon/mt8163/BoardConfigVendor.mk
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1280
