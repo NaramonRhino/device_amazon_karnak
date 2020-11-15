deb02c040b223781f048187de571947125d23726
NaramonRhino <ultimate.nafis.games@gmail.com>
2020-11-15 10:33:16 +0600

    Fix more vendor paths

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,8 +19,7 @@ DEVICE_PATH := device/amazon/karnak
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Inherit from the proprietary version
-include vendor/amazon/mt8163/BoardConfigVendor.mk
-include vendor/amazon/karnak/BoardConfigVendor.mk
+include vendor/amazon/BoardConfigVendor.mk
 
 # Bootanimation
 TARGET_SCREEN_WIDTH := 1280
