0edded31437bf3bdb16c4a056298b216bc5d8c19
NaramonRhino <ultimate.nafis.games@gmail.com>
2020-11-15 10:30:00 +0600

    Fix vendor paths

--- device.mk
+++ device.mk
@@ -216,8 +216,8 @@ include $(LOCAL_PATH)/hidl.mk
 # System Prop
 include $(LOCAL_PATH)/system_prop.mk
 
-include vendor/amazon/karnak/karnak-vendor.mk
-include vendor/amazon/mt8163/mt8163-vendor.mk
+include vendor/amazon/BoardConfigVendor.mk
+include vendor/amazon/mt8163-vendor.mk
 
 # call dalvik heap config
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
